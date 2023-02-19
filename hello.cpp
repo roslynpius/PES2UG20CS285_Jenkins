2af23da63b3e60c026fe5ef55a5ea58d2926dfc0
roslynpius <79206594+roslynpius@users.noreply.github.com>
2023-02-19 10:55:14 +0530

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Task-5\n";
+  cout <<"Aim: To create a basic Jenkins pipeline\n";
+  
+  return 0;
+}
